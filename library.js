196e7568d721dd64d91d94390c99f38a5143c5dd
pohjie <0xArlet@gmail.com>
2022-12-15 19:27:49 +0800

    Set up Prettier

---
+++ library.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    return (
+      `${this.title} by ${this.author}, ${this.pages} pages, ${
+        this.read ? "has read" : "not read yet"
+      }`
+    );
+  };
+}
+
+function addBooktoLibrary() {}
