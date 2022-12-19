a3a53196d7540b03a9e01da3636f6573e0f2bec5
pohjie <0xArlet@gmail.com>
2022-12-19 12:07:05 +0800

    Display book cards in grid

--- library.js
+++ library.js
@@ -18,3 +18,34 @@ function Book(title, author, pages, read) {
 function addBooktoLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
+
+const display = document.querySelector('.display');
+const loopArrDisplayBook = () => {
+  myLibrary.forEach(book => {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+
+    const bookTitle = document.createElement('h3');
+    bookTitle.textContent = book.title;
+    bookCard.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.textContent = book.author;
+    bookCard.appendChild(bookAuthor);
+
+    const bookNumPages = document.createElement('p');
+    bookNumPages.textContent = book.pages;
+    bookCard.appendChild(bookNumPages);
+
+    const hasRead = document.createElement('p');
+    hasRead.textContent = book.read ? "Has read" : "Not fully read";
+    bookCard.appendChild(hasRead);
+
+    display.appendChild(bookCard);
+  })
+}
+
+// Hardcoded to test validity of loopArrDisplayBook
+addBooktoLibrary("Lord of the Rings", "J.R.R Tolkkien", "340", true);
+addBooktoLibrary("Attack on Titan", 'Hajime Isayama', '162', false);
+loopArrDisplayBook();
